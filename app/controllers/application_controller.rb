52ba85067e4ea4209f97f3eff36244dea36abd8c
Victoria Meng <nights1ststar@protonmail.com>
2018-05-29 01:39:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,34 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    erb :index
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  post '/posts' do
+    Post.create(params)
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params['id'])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+  end
+
+
 end
