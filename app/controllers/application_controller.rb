4e95cbe60cd2eaf64d74815337fd134bb0283762
Victoria Meng <nights1ststar@protonmail.com>
2018-05-29 02:06:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,6 @@ class ApplicationController < Sinatra::Base
 
   patch '/posts/:id' do
     @post = Post.find(params['id'])
-    binding.pry
     @post.update(name: params['name'], content: params['content'])
     redirect to '/posts/#{@post.id}'
   end
